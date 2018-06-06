37ea233864e405de8a14933661b7d8392ce8ea81
Jessica Brush <jessicadawnbrush@gmail.com>
2018-06-05 20:18:13 -0700

    logic v1

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,11 +5,18 @@
 // But you could have it be an empty array as well.
 // ===============================================================================
 
-var friendArray = [{
+var friendArray = [
+    {
     name: "Joe",
     photo: "https://www.liveaction.org/news/wp-content/uploads/2013/09/000friends.jpg",
-    scores: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
-}];
+    scores: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ],
+},
+{
+    name: "Sam",
+    photo: "https://www.lovedignity.com/wp-content/uploads/2016/07/friendship.jpg",
+    scores: [ 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 ],
+    }
+];
 
 // Note how we export the array. This makes it accessible to other files using require.
 module.exports = friendArray;
